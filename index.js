2bc56572c6bfab09224879f75a17d60e8df9cc69
okantas <okantas954@gmail.com>
2024-04-10 00:50:15 +0300

    gsap is implemented

---
+++ index.js
@@ -0,0 +1,16 @@
+const t1 = gsap.timeline();
+
+t1.from(".crescent", {
+  opacity: 0,
+  y: 400,
+  duration: 1.3,
+})
+  .from(".mosque", {
+    opacity: 0,
+    y: -200,
+    duration: 1.3,
+  })
+  .from([".firstWord", ".secondWord"], {
+    opacity: 0,
+    duration: 1.3,
+  });
